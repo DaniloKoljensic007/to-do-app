9689282ae2314b8b596edbd454ed7b00e99e5fe5
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-17 18:28:43 +0200

    Adding Add Task form and view

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add/", views.add_task, name="add"),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
@@ -9,3 +9,10 @@ def index(request):
     tasks = Task.objects.all().order_by("-created_at")
 
     return render(request, "index.html", {"tasks": tasks})
+
+
+def add_task(request):
+    title = request.POST["title"]
+    Task.objects.create(title=title)
+
+    return redirect("index")
