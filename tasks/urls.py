5e2b3f97677d17c0cf13dc6f23c093a3709b693c
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-17 19:00:57 +0200

    Added delete view and form to be able to delete tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.add_task, name="add"),
     path("<int:task_id>/update", views.update_task, name="update"),
+    path("<int:task_id>/delete", views.delete_task, name="delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -25,3 +25,11 @@ def update_task(request, task_id):
     task.save()
 
     return redirect("index")
+
+
+def delete_task(request, task_id):
+
+    task = get_object_or_404(Task, pk=task_id)
+    task.delete()
+
+    return redirect("index")
