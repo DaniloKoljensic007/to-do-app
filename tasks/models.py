354fb12f97396f437a00b9aff0287b3fde1fa766
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-17 17:58:31 +0200

    Added Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    created_at = models.DateTimeField("Created", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated", auto_now=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
