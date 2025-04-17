328efcb612654ca811182e295c5d4fadc29d8f72
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-17 18:16:55 +0200

    Added endpoint for showing tasks and form to add new

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Task
+
 # Create your views here.
 
 
 def index(request):
-    
+
+    tasks = Task.objects.all().order_by("-created_at")
+
+    return render(request, "index.html", {"tasks": tasks})
